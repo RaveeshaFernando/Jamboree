3f1a1f312c4a704c7258d0400f2222cb2fc36d2f
Raveesha Fernando <rdilshanf@gmail.com>
2019-09-14 10:30:41 +0530

    front end home finalised

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
-import { FeedComponent } from './feed/feed.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FootComponent } from './foot/foot.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    FeedComponent,
-    NavbarComponent,
-    FootComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        SigninComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        NgbModule.forRoot(),
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        ComponentsModule,
+        ExamplesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
