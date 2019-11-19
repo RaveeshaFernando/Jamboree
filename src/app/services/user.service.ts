2ae3324c76219a2b9e2b0dbb7019472a43a63272
KDShanika <abcshani1995@gmail.com>
2019-11-19 10:33:46 +0530

    retrieve data

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 import { User } from '../User';
 
@@ -9,8 +10,16 @@ export class UserService {
   Users: Observable<User[]>;
   
   constructor(public afs: AngularFirestore) {
-    this.Users = this.afs.collection('Users').valueChanges();
-   }
+    //this.Users = this.afs.collection('Users').valueChanges();
+    //getting access to the id valueChanges-->sanpshotChanges
+    this.Users = this.afs.collection('Users').snapshotChanges().pipe(map(changes =>{
+      return changes.map(a => {
+        const data = a.payload.doc.data() as User;
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    }))
+  }
 
   getUsers(){
     return this.Users;
