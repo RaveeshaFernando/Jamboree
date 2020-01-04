b93a3baee84abb45d42f6e5e061f2ce3dfb3611e
KDShanika <abcshani1995@gmail.com>
2020-01-04 15:57:48 +0530

    user profile

--- src/app/User/Shared/user-sidebar/user-sidebar.component.ts
+++ src/app/User/Shared/user-sidebar/user-sidebar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SampleUserService } from 'src/app/BackendConfig/sample-user.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/BackendConfig/user.model';
+import { AuthService } from "../../../BackendConfig/auth.service";
 
 @Component({
   selector: 'app-user-sidebar',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-sidebar.component.scss']
 })
 export class UserSidebarComponent implements OnInit {
+  Users: User[];
+  flag: Boolean
+  Log: any
+  
 
-  constructor() { }
+  constructor(private userService: SampleUserService, public authService : AuthService) { }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(dataArray => {
+      this.Users = dataArray.map(item =>{
+        return {id : item.payload.doc.id,
+        ...item.payload.doc.data()
+      } as User
+      })
+      })
+
+      this.authService.authenticated.subscribe(isAuthed => {
+        this.flag = isAuthed;
+        this.Log = this.authService.GetUserData().subscribe(user => {
+          this.Log = user
+        });
+      });
   }
+
 
 }
