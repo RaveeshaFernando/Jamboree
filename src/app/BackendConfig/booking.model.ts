f6515d9b555202d38daec507dd812b2253647836
shanika waidyaratne <shanikawaidyarathne@gmail.com>
2020-02-14 08:23:04 +0530

    minor changes in user booking

--- src/app/BackendConfig/booking.model.ts
+++ src/app/BackendConfig/booking.model.ts
@@ -3,6 +3,7 @@ export class Booking {
     date : string;
     eventComplete : string;
     eventDisplayName : string;
+    eventType : string;
     status : string;
     userComplete : string;
     userDisplayName : string;
